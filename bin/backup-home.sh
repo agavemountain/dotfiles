c61ecef8787ef86441fee8769499504416858d8f
Joe Turner <joe@agavemountain.com>
2019-11-27 12:21:37 -0600

    Add exclusions to backup

--- bin/backup-home.sh
+++ bin/backup-home.sh
@@ -2,6 +2,11 @@
 #
 # backup home directory to the specified destination
 #
+# This is a quick and dirty script to backup my home directory
+# before I nuke this machine and install a fresh copy of 
+# a different OS.
+#
+
 NOW=$(date +"%m-%d-%Y")
 DEST=~/nas/DATA/backups/backup-home-$USER-$NOW
 
@@ -9,4 +14,10 @@ echo "backing up $HOME to: $DEST"
 
 mkdir -p $DEST
 
-sudo rsync -aAXv $HOME --exclude={"$HOME/nas/*"} $DEST
+sudo rsync -aAXv $HOME --exclude="nas/*" --exclude="Downloads/*" \
+    --exclude=".cache/*" --exclude=".caches/*" \
+    --exclude=".config/*" --exclude ".hunter/*" \
+    --exclude=".linuxbrew/*" --exclude=".local/*" \
+    --exclude=".p2/*" --exclude=".mozilla/*" --exclude=".m2/*" \
+    --exclude=".eclipse/*"  --exclude=".npm/*" --exclude=".vscode/*" \
+    $DEST
